6e2cc455ca1f762b71628d35afc60e6ce1360de2
Pongsapat1035 <pongsapat357@gmail.com>
2025-01-20 17:44:27 +0700

    init project

---
+++ app/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div>
+            This is dash board
+        </div>
+    )
+}
---
+++ app/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHdG7_k4fLvQ6SAAiSLOLx3Q8-X-9PNbE",
+    authDomain: "finance-app-fc057.firebaseapp.com",
+    projectId: "finance-app-fc057",
+    storageBucket: "finance-app-fc057.firebasestorage.app",
+    messagingSenderId: "548963231458",
+    appId: "1:548963231458:web:d1a6b6792535282e32e0bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+    db
+}
